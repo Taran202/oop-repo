10dfb90a7cc5f03f1aaa451dfe1ada348832c5fa
Taran202 <112495@emmauscollege.nl>
2023-09-05 10:29:27 +0000

    kalr

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
- /* Opdracht Objectgeorienteerd programmeren
-    Informatica - Emmauscollege Rotterdam
- */
+/* Opdracht Objectgeorienteerd programmeren
+   Informatica - Emmauscollege Rotterdam
+*/
 
 /* ******************************************************* */
 /* instellingen om foutcontrole van je code beter te maken */
@@ -12,7 +12,7 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
-var xPosities ;
+var xPosities;
 var yPosities;
 var speedsX;
 var speedsY;
@@ -34,12 +34,11 @@ function setup() {
   createCanvas(1280, 720);
 
   // initialiseer waarden
-  x = width / 2;               // midden van de breedte van het canvas
-  y = height / 2;              // midden van de hoogte van het canvas
-  speedsX = random[30, 20,50,-10,80];      // random waarde tussen -5 en 5
-  speedsY = random[-60, 10,-20,40,25];  
-  xPosities= [130, 60, 470,30,80];
-  yPosities=[700, 1010, 40,50,70]    // 👆
+
+  speedsX = [3, 2, 5, -1, 8];      // random waarde tussen -5 en 5
+  speedsY = [-6, 1, -2, 4, 2];
+  xPosities = [130, 60, 470, 300, 80];
+  yPosities = [70, 101, 410, 510, 160]    // 👆
 }
 
 /**
@@ -50,25 +49,25 @@ function setup() {
 function draw() {
   // zwarte achtergrond
   background(0, 0, 0);
-//alle posities arra
-for (var i = 0; i < xPosities.length; i++) {
-  // teken
-  noStroke;
-  fill(255, 255, 255);
-  rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
+  //alle posities arra
+  for (var i = 0; i < xPosities.length; i++) {
+    // teken
+    noStroke;
+    fill(255, 255, 255);
+    rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
 
-  // update positie
-  xPosities= xPosities[i]+ speedsX[i];
-  yPosities= yPosities[i]+ speedsY[i];
+    //  posities
+    xPosities[i] = xPosities[i] + speedsX[i];
+    yPosities[i] = yPosities[i] + speedsY[i];
 
-  // stuiter evt. tegen de kanten
-  if (xPosities[i] <= 0 || xPosities[i] + BREEDTE >= width) {
-    speedsX[i] = speedsX[i] * -1;
+    // weg stuiter
+    if (xPosities[i] <= 0 || xPosities[i] + BREEDTE >= width) {
+      speedsX[i] = speedsX[i] * -1;
+    }
+
+    if (yPosities[i] <= 0 || yPosities[i] + BREEDTE >= height) {
+      speedsY[i] = speedsY[i] * -1;
+    }
+
   }
-
-  if (yPosities[i] <= 0 || yPosities[i] + BREEDTE >= height) {
-    speedsY[i] = speedsY[i] * -1;
-  }
-
-}
 }
