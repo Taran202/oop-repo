6a116607603bb3e134609f46e34c9580a8bc09ec
Taran202 <112495@emmauscollege.nl>
2023-09-15 07:15:58 +0000

    opdr 2

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var yPosities;
 var speedsX;
 var speedsY;
 const BREEDTE = 20;
+var mensen;
 
 
 
@@ -34,13 +35,12 @@ function setup() {
   createCanvas(1280, 720);
 
   // initialiseer waarden
-
-  speedsX = [3, 2, 5, -1, 8];      // random waarde tussen -5 en 5
-  speedsY = [-6, 1, -2, 4, 2];
-  xPosities = [130, 60, 470, 300, 80];
-  yPosities = [70, 101, 410, 510, 160]    // 👆
-}
-
+  mensen = [ { x: 130, y: 650, speedX: 2,  speedY: 1  },
+    { x: 34,  y: 478, speedX: -6, speedY: -4 },
+    { x: 420, y: 323,  speedX: 8,  speedY: -6 },
+    { x: 340, y: 123, speedX: -2, speedY: -2 },
+    { x: 800, y: 268,  speedX: -3, speedY: 5  } ];
+} 
 /**
  * draw
  * de code in deze functie wordt 50 keer per seconde
@@ -50,23 +50,27 @@ function draw() {
   // zwarte achtergrond
   background(0, 0, 0);
   //alle posities arra
-  for (var i = 0; i < xPosities.length; i++) {
+  for (var i = 0; i < Mensen.length; i++) {
+   var eenMens= Mensen[i]
     // teken
     noStroke;
     fill(255, 255, 255);
-    rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
+    rect(eenMens.x [i], eenMens.y[i], BREEDTE, BREEDTE);
+   //  posities
 
-    //  posities
-    xPosities[i] = xPosities[i] + speedsX[i];
-    yPosities[i] = yPosities[i] + speedsY[i];
+    eenMens.x = eenMens.x + eenMens.speedX;
+    eenMens.y = eenMens.y + eenMens.speedY;
+
+ 
+    
 
     // weg stuiter
-    if (xPosities[i] <= 0 || xPosities[i] + BREEDTE >= width) {
-      speedsX[i] = speedsX[i] * -1;
+    if (eenMens.x <= 0 || eenMens.speedX+ BREEDTE >= width) {
+      eenMens.speedsX = eenMens.speedsX * -1;
     }
 
-    if (yPosities[i] <= 0 || yPosities[i] + BREEDTE >= height) {
-      speedsY[i] = speedsY[i] * -1;
+    if (eenMens.y <= 0 || eenMens.y + BREEDTE >= height) {
+      eenMens.speedY = eenMens.speedY * -1;
     }
 
   }
